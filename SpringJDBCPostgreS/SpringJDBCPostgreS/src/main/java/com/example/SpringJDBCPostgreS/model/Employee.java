c97f7db97d13e7f3a60be6c7dbc9528619abdb5a
KavishkaSasindu <sasindu0215@gmail.com>
2025-02-09 09:43:51 +0530

    created employee model

--- SpringJDBCPostgreS/SpringJDBCPostgreS/src/main/java/com/example/SpringJDBCPostgreS/model/Employee.java
+++ SpringJDBCPostgreS/SpringJDBCPostgreS/src/main/java/com/example/SpringJDBCPostgreS/model/Employee.java
@@ -1,4 +1,65 @@
 package com.example.SpringJDBCPostgreS.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Employee {
+
+    private int employeeId;
+    private String employeeName;
+    private String employeeRoll;
+    private int employeeSalary;
+
+    public Employee() {
+        System.out.println("Employee Constructor");
+    }
+
+    public Employee(int employeeId, String employeeName, String employeeRoll, int employeeSalary) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.employeeRoll = employeeRoll;
+        this.employeeSalary = employeeSalary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeRoll() {
+        return employeeRoll;
+    }
+
+    public void setEmployeeRoll(String employeeRoll) {
+        this.employeeRoll = employeeRoll;
+    }
+
+    public int getEmployeeSalary() {
+        return employeeSalary;
+    }
+
+    public void setEmployeeSalary(int employeeSalary) {
+        this.employeeSalary = employeeSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", employeeName='" + employeeName + '\'' +
+                ", employeeRoll='" + employeeRoll + '\'' +
+                ", employeeSalary=" + employeeSalary +
+                '}';
+    }
 }
